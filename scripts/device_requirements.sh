42c5e33121cb7d4e37da073c8ae4945ffcaa48c4
env. <162432464+belowzeroiq@users.noreply.github.com>
2025-12-11 21:24:07 +0700

    Update device_requirements.sh

--- scripts/device_requirements.sh
+++ scripts/device_requirements.sh
@@ -3,6 +3,19 @@
 set_requirements() {
 rm -rf vendor/lineage
 git clone --depth=1 https://github.com/topnotchfreaks/android_vendor_lineage -b lineage-20.0 vendor/lineage
+
+
+# Clone Topaz/Tapas Trees
+git clone --depth=1 https://github.com/topnotchfreaks/device_xiaomi_topaz -b lineage-20 device/xiaomi/topaz
+git clone --depth=1 https://github.com/topnotchfreaks/vendor_xiaomi_topaz -b lineage-20 vendor/xiaomi/topaz
+git clone --depth=1 https://github.com/topnotchfreaks/device_xiaomi_topaz-kernel -b lineage-20 device/xiaomi/topaz-kernel
+
+# Clone required files
+git clone --depth=1 https://github.com/topnotchfreaks/hardware_qcom-caf_sm6225 -b lineage-20.0 hardware/qcom-caf/sm6225
+
+# Clone Hardware_xiaomi
+rm -rf hardware/xiaomi
+git clone --depth=1 https://github.com/topnotchfreaks/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
 }
 
 set_requirements
